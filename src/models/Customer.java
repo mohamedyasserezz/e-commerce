ccdfc1b54b048a0d6b7b1e8c6727049b5898ebd3
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-07-04 23:27:45 +0300

    add customer class

---
+++ src/models/Customer.java
@@ -0,0 +1,23 @@
+package models;
+
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deductBalance(double amount) {
+        this.balance -= amount;
+    }
+} 
