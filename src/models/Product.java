1b7f6065aed7fb01740999ca9ead7dfb7586e0e5
Mohamed Yasser <mohamedyasserezz067@gmail.com>
2025-07-04 23:24:44 +0300

    Add Product class

---
+++ src/models/Product.java
@@ -0,0 +1,29 @@
+package models;
+
+public abstract class Product {
+    protected String name;
+    protected double price;
+    protected int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void reduceQuantity(int amount) {
+        this.quantity -= amount;
+    }
+} 
